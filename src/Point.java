c2acc2a6d2b187682c30518ef1667fb1d3fc851f
connorbensyl <48489225+connorbensyl@users.noreply.github.com>
2019-09-05 23:01:39 -0500

    added constructor and some spaghetti code to point class

--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,23 @@
 
-public class Point {
+public class Point 
+{
+ private
+	 boolean isCorner;
+     boolean isMidpoint;
+ public
+ 
+ Point(boolean iscorner, boolean ismidpoint)
+ {
+	 
+	 isCorner = iscorner;
+	 isMidpoint = ismidpoint;
+ }
+ 
+ public boolean get_isCorner()
+ {
+	 return isCorner;
+ }
+ 
+ 
 
 }
