51a881c88150d19bf8f7fcd633666523dadf865a
Kendal <krs8cd@mail.umkc.edu>
2019-09-13 15:39:45 -0500

    changes

--- src/sprint1/Board.java
+++ src/sprint1/Board.java
@@ -3,22 +3,16 @@ package sprint1;
 public class Board 
 {
 
-	private static final int TotalRows = 7;
-	private static final int TotalColumns = 7;
-	public enum Cell{
-		EMPTY, CROSS, NOUGHT
+	private Board board = new Board();
+	
+	public void setUp() throws Exception{
 	}
 	
-	private Cell [][]grid;
-	
-	public Board() {
-		grid = new Cell[TotalRows][TotalColumns];
-		initBoard();
+	public void tearDown() throws Exception{
 	}
 	
-	public void initBoard() {
-		for (int row = 0; row < TotalRows; ++row) {
-			
-		}
-	}
+	
+	
+	
+	
 }
