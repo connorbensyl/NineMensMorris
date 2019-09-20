bf5503d2bae39f9a2811c8582def8655ca270be6
Kendal <krs8cd@mail.umkc.edu>
2019-09-20 15:41:25 -0500

    change

--- src/sprint1/Player1Moves.java
+++ src/sprint1/Player1Moves.java
@@ -5,7 +5,13 @@ import sprint1.Board;
 
 public class Player1Moves {
 
-	private Board board;
+private Board board = new Board();
 	
+	public void setUp() throws Exception{
+		board = new Board();
+	}
+	
+	public void tearDown() throws Exception{
+	}
 	
 }
--- src/sprint1/Player2Moves.java
+++ src/sprint1/Player2Moves.java
@@ -5,4 +5,13 @@ import sprint1.Board;
 
 public class Player2Moves {
 
+private Board board = new Board();
+	
+	public void setUp() throws Exception{
+		board = new Board();
+	}
+	
+	public void tearDown() throws Exception{
+	}
+	
 }
