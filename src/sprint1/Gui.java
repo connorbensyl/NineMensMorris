5156e40e6dc2353bb85471a37e3d40a6811a5c70
smhrd3 <armorsteven21@yahoo.com>
2019-09-20 13:22:27 -0500

    latest update

--- src/sprint1/Gui.java
+++ src/sprint1/Gui.java
@@ -2,16 +2,15 @@ package sprint1;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 import java.awt.GridBagLayout;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class Gui {
 
@@ -49,30 +48,37 @@ public class Gui {
 	private void initialize() {
 		NineMensMorris = new JFrame();
 		NineMensMorris.setTitle("Nine Men's Morris");
-		NineMensMorris.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		NineMensMorris.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		NineMensMorris.setVisible(true);
 		NineMensMorris.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		
 		NineMensMorrisBG = new JPanel();
-		NineMensMorrisBG.setBackground(Color.WHITE);
+		NineMensMorrisBG.setBounds(0, 0, 1920, 1080);
+		NineMensMorrisBG.setBackground(new Color(240, 255, 240));
 		NineMensMorris.getContentPane().add(NineMensMorrisBG);
 		
 		JButton PVPButton = new JButton("Player VS Player");
+		PVPButton.setForeground(Color.RED);
 		PVPButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		PVPButton.setBounds(250,250,120,35);
+		NineMensMorrisBG.setLayout(null);
+		
+		PVPButton.setBounds(392,197,147,29);
 		NineMensMorrisBG.add(PVPButton);
 		
 		JButton PVAIButton = new JButton("Player VS AI");
+		PVAIButton.setBounds(404, 259, 121, 29);
+		PVAIButton.setForeground(Color.MAGENTA);
 		PVAIButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		
 		NineMensMorrisBG.add(PVAIButton);
 		
+
 
 	}
 
