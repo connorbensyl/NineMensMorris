eee3a6a37cb18a70a000b9614cce39ce05401c0d
Kendal <krs8cd@mail.umkc.edu>
2019-09-23 15:51:59 -0500

    board test folder

---
+++ src/sprint1/BoardTesting.java
@@ -0,0 +1,14 @@
+package sprint1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoardTesting {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
