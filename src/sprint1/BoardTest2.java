50ceef19c428f1483a39136763fdde82bba0cc68
connorbensyl <48489225+connorbensyl@users.noreply.github.com>
2019-10-02 15:48:50 -0500

    added placePiece function in controller

---
+++ src/sprint1/BoardTest2.java
@@ -0,0 +1,39 @@
+package sprint1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BoardTest2 {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSetUp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTearDown() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConnectIntersquarePoints() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConnectPoints() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/sprint1/Controller.java
+++ src/sprint1/Controller.java
@@ -4,8 +4,29 @@ public class Controller {
 
 	public static void main(String[] args) 
 	{
+		Board board = new Board();
+		Piece Player1_Pieces = new Piece("black");
+		Piece Player2_Pieces = new Piece("white");
+		
 		// TODO Auto-generated method stub
 
+	}
+	
+	public String placePiece(Piece player, Point location)
+	{
+		
+		if(!location.isEmpty())
+		{
+			return "Point is already occupied by a piece";
+		}
+		else
+		{
+			String color_of_piece = location.get_point_state();
+			location.set_point_state(color_of_piece);
+			player.removePiece();
+			return "Piece placed";
+		}
+		
 	}
 
 }
