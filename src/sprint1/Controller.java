91eab49009179f651dc5288fec5157d4a0a9cc3a
connorbensyl <48489225+connorbensyl@users.noreply.github.com>
2019-10-02 16:04:54 -0500

    made member variables for controller global and private

--- src/sprint1/Controller.java
+++ src/sprint1/Controller.java
@@ -2,13 +2,20 @@ package sprint1;
 
 public class Controller {
 
+	private String Phase;
+	Board board = new Board();
+	Piece Player1_Pieces = new Piece("black");
+	Piece Player2_Pieces = new Piece("white");
+	Player player1;
+	Player player2;
+	
 	public static void main(String[] args) 
 	{
-		Board board = new Board();
-		Piece Player1_Pieces = new Piece("black");
-		Piece Player2_Pieces = new Piece("white");
 		
-		// TODO Auto-generated method stub
+		board.connectPoints(); //need to change this to connect all local points
+		board.connectIntersquarePoints();
+		
+		
 
 	}
 	
@@ -23,10 +30,36 @@ public class Controller {
 		{
 			String color_of_piece = location.get_point_state();
 			location.set_point_state(color_of_piece);
-			player.removePiece();
+			player.decrement_unplayed_pieces();
 			return "Piece placed";
+			
 		}
 		
+	}
+	
+	Board get_controller_Board()
+	{
+		return board;
+	}
+	
+	Piece getplayer1pieces()
+	{
+		return Player1_Pieces;
+	}
+	
+	Piece getplayer2pieces()
+	{
+		return Player2_Pieces;
+	}
+	
+	Player getPlayer1()
+	{
+		return player1;
+	}
+	
+	Player getPlayer2()
+	{
+		return player2;
 	}
 
 }
