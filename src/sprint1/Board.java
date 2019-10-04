5e4d455012d1e7408552cff8f8ab6806bd58f036
connorbensyl <48489225+connorbensyl@users.noreply.github.com>
2019-10-04 18:11:45 -0500

    unfinished set board coordinates function

--- src/sprint1/Board.java
+++ src/sprint1/Board.java
@@ -45,14 +45,25 @@ public class Board
 	}
 	
 	public void setSquareCoords()
-	{
+	{   
+		
+		//coordinates for points on outer square
 		outerSquare.topLeft.set_coords(350, 70);
 		outerSquare.middleLeft.set_coords(350, 320);
 		outerSquare.bottomLeft.set_coords(350, 570);
 		outerSquare.bottomMiddle.set_coords(600, 580);
 		outerSquare.bottomRight.set_coords(850,580);
 		outerSquare.middleRight.set_coords(670,320);
-		outerSquare.topRight.set_coords(, y);
+		outerSquare.topRight.set_coords(850, 70);
+		outerSquare.topMiddle.set_coords(600, 70);
+		
+		//coordinates for points on middle square
+		middleSquare.topLeft.set_coords(450, 170);
+		middleSquare.middleLeft.set_coords(450, 320);
+		middleSquare.bottomLeft.set_coords(450, 470);
+		middleSquare.bottomMiddle.set_coords(600,490);
+		middleSquare.bottomRight.set_coords(750,480);
+		middleSquare.middleRight.set_coords(x, y);
 	}
 	
 	
