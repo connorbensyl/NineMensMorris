f4888ea88474d0b44094f41828adb96487148caa
connorbensyl <48489225+connorbensyl@users.noreply.github.com>
2019-11-21 01:35:54 -0600

    some things
    determine x and y values of the coordinates of point on 2D array from raw mouse click coordinates from gui

--- src/alternative_with_2D_Array/Controller.java
+++ src/alternative_with_2D_Array/Controller.java
@@ -86,12 +86,77 @@ public void control_game(Gui1 controllerGui)
 		}
 	}
 }
-public boolean has_valid_move(Player p) //check if player of color still has valid move and thus is still in the game
+public int getxCoord(int xgui) //take in raw coordinate x value from gui
 {
-	
-	
-	
-	return false;
+	if(xgui > 330 && xgui < 850) 
+	{
+		if(xgui > 330 && xgui < 430)//if x is in 0th column
+		{
+			return 0; //so the xvalue on the grid of the point that was clicked is 0
+		}
+		else if(xgui > 430 && xgui < 509)//if x is in 1st column...etc. I add some margin of error so it's not exactly the same as the point coordinates, this will probably need to be adjusted
+		{
+			return 1; 
+		}
+		else if(xgui > 509 && xgui < 580)
+		{
+			return 2;
+		}
+		else if(xgui > 580 && xgui < 650)
+		{
+			return 3;
+		}
+		else if(xgui > 650 && xgui < 730)
+		{
+			return 4;
+		}
+		else if(xgui > 730 && xgui < 830)
+		{
+			return 5;
+		}
+		else if(xgui > 830 && xgui < 850)
+		{
+			return 6;
+		}
+	}
+	return -1; // if -1 is returned then they clicked outside the bounds to the left or right
+}
+
+public int getyCoord(int ygui)
+{
+	if(ygui < 580 && ygui > 30)
+	{
+		if(ygui > 450 && ygui < 580)
+		{
+			return 0;
+		}
+		else if(ygui > 377 && ygui < 450)
+		{
+			return 1;
+		}
+		else if(ygui > 300 && ygui < 377)
+		{
+			return 2;
+		}
+		else if(ygui > 230 && ygui < 300)
+		{
+			return 3;
+		}
+		else if(ygui > 150 && ygui < 230)
+		{
+			return 4;
+		}
+		else if(ygui > 50 && ygui < 150)
+		{
+			return 5;
+		}
+		else if(ygui > 30 && ygui < 50)
+		{
+			return 6;
+		}
+		
+	}
+	return -1; //if -1 is returned then the player clicked outside the bounds on the top or bottom
 }
 
 }
