8233df2f1e11991a25e10e3bd58acf9d243355ab
Kendal <krs8cd@mail.umkc.edu>
2019-11-30 00:33:07 -0600

    AI

---
+++ src/alternative_with_2D_Array/AI.java
@@ -0,0 +1,21 @@
+package alternative_with_2D_Array;
+
+import alternative_with_2D_Array.Board;
+
+public class AI {
+
+	public int moves = 0;
+	public int remove = 0;
+	String name = "Computer";
+	public boolean playerAI = false;
+	
+	public String setName() {
+		return name;
+	}
+	
+	public boolean playerAI() {
+		return true;
+	}
+	
+	
+}
